06b42907197cdc485ff2e1e9d6d653dfc6def318
Gerolsteiner <solid.absorption@gmail.com>
2024-11-26 23:36:21 +0900

    register画面作成中

--- src/database/migrations/2014_10_12_000000_create_users_table.php
+++ src/database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,13 +14,14 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
+            $table->id(); // 主キー
+            $table->string('name'); // 名前
+            $table->string('email')->unique(); // メールアドレス（ユニーク）
+            $table->string('password'); // パスワード
+            $table->timestamp('email_verified_at')->nullable(); // メール認証
+            $table->tinyInteger('is_admin')->default(0); // is_adminカラム（デフォルトは0）
+            $table->timestamps(); // created_at と updated_at
+
         });
     }
 
